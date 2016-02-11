949a696c87bb543a7c3745e9193a597ea4185179
Stephen Gallagher <sgallagh@redhat.com>
2016-02-11 16:26:14 -0500

    Add tests for multiple IDPs

--- test/integration/web_console_access_test.go
+++ test/integration/web_console_access_test.go
@@ -3,10 +3,15 @@
 package integration
 
 import (
+	"bytes"
 	"crypto/tls"
 	"encoding/json"
+	"fmt"
+	"html/template"
 	"io/ioutil"
 	"net/http"
+	"net/url"
+	"regexp"
 	"strings"
 	"testing"
 
@@ -17,7 +22,22 @@ import (
 	testserver "github.com/openshift/origin/test/util/server"
 )
 
-func tryAccessURL(t *testing.T, url string, expectedStatus int, expectedRedirectLocation string) *http.Response {
+func templateEscape(s string) (string, error) {
+	temp := new(bytes.Buffer)
+
+	t, err := template.New("foo").Parse(`{{define "T"}}{{.}}{{end}}`)
+	if err != nil {
+		return "", err
+	}
+	err = t.ExecuteTemplate(temp, "T", s)
+	if err != nil {
+		return "", err
+	}
+
+	return temp.String(), nil
+}
+
+func tryAccessURL(t *testing.T, url string, expectedStatus int, expectedRedirectLocation string, expectedLinks []string) *http.Response {
 	transport := knet.SetTransportDefaults(&http.Transport{
 		TLSClientConfig: &tls.Config{
 			InsecureSkipVerify: true,
@@ -38,8 +58,25 @@ func tryAccessURL(t *testing.T, url string, expectedStatus int, expectedRedirect
 	location := resp.Header.Get("Location")
 	location = strings.SplitN(location, "?", 2)[0]
 	if location != expectedRedirectLocation {
-		t.Errorf("Expected redirecttion to %q for %q, got %q instead", expectedRedirectLocation, url, location)
+		t.Errorf("Expected redirection to %q for %q, got %q instead", expectedRedirectLocation, url, location)
 	}
+
+	if expectedLinks != nil {
+		body, err := ioutil.ReadAll(resp.Body)
+		if err != nil {
+			t.Errorf("failed to read reposponse's body: %v", err)
+		} else {
+			for _, linkRegexp := range expectedLinks {
+				matched, err := regexp.Match(linkRegexp, body)
+				if err != nil {
+					t.Errorf("unexpected error: %v", err)
+				} else if !matched {
+					t.Errorf("Expected a link matching %q in response body.", linkRegexp)
+				}
+			}
+		}
+	}
+
 	return resp
 }
 
@@ -66,7 +103,7 @@ func TestAccessOriginWebConsole(t *testing.T) {
 		"console/java":        {http.StatusOK, ""},
 	} {
 		url := masterOptions.AssetConfig.MasterPublicURL + "/" + endpoint
-		tryAccessURL(t, url, exp.statusCode, exp.location)
+		tryAccessURL(t, url, exp.statusCode, exp.location, nil)
 	}
 }
 
@@ -81,7 +118,7 @@ func TestAccessDisabledWebConsole(t *testing.T) {
 		t.Fatalf("unexpected error: %v", err)
 	}
 
-	resp := tryAccessURL(t, masterOptions.AssetConfig.MasterPublicURL+"/", http.StatusOK, "")
+	resp := tryAccessURL(t, masterOptions.AssetConfig.MasterPublicURL+"/", http.StatusOK, "", nil)
 	body, err := ioutil.ReadAll(resp.Body)
 	if err != nil {
 		t.Errorf("failed to read reposponse's body: %v", err)
@@ -104,6 +141,100 @@ func TestAccessDisabledWebConsole(t *testing.T) {
 		"console/java":        {http.StatusForbidden, ""},
 	} {
 		url := masterOptions.AssetConfig.MasterPublicURL + "/" + endpoint
-		tryAccessURL(t, url, exp.statusCode, exp.location)
+		tryAccessURL(t, url, exp.statusCode, exp.location, nil)
+	}
+}
+
+func TestAccessOriginWebConsoleMultipleIdentityProviders(t *testing.T) {
+	testutil.RequireEtcd(t)
+	masterOptions, err := testserver.DefaultMasterOptions()
+	if err != nil {
+		t.Fatalf("unexpected error: %v", err)
+	}
+
+	// Replace the default IdentityProvider with an AllowAll provider
+	masterOptions.OAuthConfig.IdentityProviders[0] = configapi.IdentityProvider{
+		Name:            "foo",
+		UseAsChallenger: true,
+		UseAsLogin:      true,
+		MappingMethod:   "claim",
+		Provider:        &configapi.AllowAllPasswordIdentityProvider{},
+	}
+
+	// Set up a second AllowAll provider
+	masterOptions.OAuthConfig.IdentityProviders = append(masterOptions.OAuthConfig.IdentityProviders, configapi.IdentityProvider{
+		Name:            "bar",
+		UseAsChallenger: true,
+		UseAsLogin:      true,
+		MappingMethod:   "claim",
+		Provider:        &configapi.AllowAllPasswordIdentityProvider{},
+	})
+
+	// Set up a third AllowAll provider with a space in the name and some
+	// unicode characters
+	masterOptions.OAuthConfig.IdentityProviders = append(masterOptions.OAuthConfig.IdentityProviders, configapi.IdentityProvider{
+		Name:            "baz qux #2",
+		UseAsChallenger: true,
+		UseAsLogin:      true,
+		MappingMethod:   "claim",
+		Provider:        &configapi.AllowAllPasswordIdentityProvider{},
+	})
+
+	// Launch the configured server
+	if _, err = testserver.StartConfiguredMaster(masterOptions); err != nil {
+		t.Fatalf("unexpected error: %v", err)
+	}
+
+	// Create a map of URLs to test
+	type urlResults struct {
+		statusCode int
+		location   string
+	}
+
+	urlMap := make(map[string]urlResults)
+	linkRegexps := make([]string, 0)
+
+	// Verify that the plain /login URI is unavailable when multiple IDPs
+	// are in use.
+	urlMap["/login"] = urlResults{http.StatusForbidden, ""}
+
+	// Create the common base URLs
+	escapedPublicURL := url.QueryEscape(masterOptions.OAuthConfig.AssetPublicURL)
+	loginSelectorBase := "/oauth/authorize?client_id=openshift-web-console&response_type=token&state=%2F&redirect_uri=" + escapedPublicURL
+
+	// Iterate through each of the providers and verify that they redirect to
+	// the appropriate login page and that the login page exists.
+	// This is done in a loop so that we can add an arbitrary additional set
+	// of providers to test.
+	for _, value := range masterOptions.OAuthConfig.IdentityProviders {
+		templateEscapedName, err := templateEscape(value.Name)
+		if err != nil {
+			t.Fatalf("unexpected error: %v", err)
+		}
+
+		queryEscapedName := template.URLQueryEscaper(templateEscapedName)
+
+		// Path to the login URI is template-escaped, then query-escaped, then has the + substituted with %20
+		loginPathName := strings.Replace(queryEscapedName, "+", "%20", -1)
+
+		urlMap[loginSelectorBase+"&idp="+queryEscapedName] = urlResults{http.StatusFound, "/login/" + loginPathName}
+		urlMap["/login/"+loginPathName] = urlResults{http.StatusOK, ""}
+
+		// The name in the regular expression needs to be template-escaped again
+		regexpName, err := templateEscape(queryEscapedName)
+		if err != nil {
+			t.Fatalf("unexpected error: %v", err)
+		}
+		linkRegexps = append(linkRegexps, fmt.Sprintf("/oauth/authorize\\?(.*)(&amp;)?idp=%s(&amp;|$)", regexp.QuoteMeta(regexpName)))
+	}
+
+	// Test the loginSelectorBase for links to all of the IDPs
+	url := masterOptions.AssetConfig.MasterPublicURL + loginSelectorBase
+	tryAccessURL(t, url, http.StatusOK, "", linkRegexps)
+
+	// Test all of these URLs
+	for endpoint, exp := range urlMap {
+		url := masterOptions.AssetConfig.MasterPublicURL + endpoint
+		tryAccessURL(t, url, exp.statusCode, exp.location, nil)
 	}
 }
